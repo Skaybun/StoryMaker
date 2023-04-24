1e665a50889588896ea58abf7c703b42833a7e1c
Ali <128646611+Skaybun@users.noreply.github.com>
2023-04-24 12:59:23 +0300

    Add files via upload

---
+++ StoryMaker/StoryMaker/Model/Story.swift
@@ -0,0 +1,15 @@
+//
+//  Story.swift
+//  StoryMaker
+//
+//  Created by Ali KINU on 24.04.2023.
+//
+
+import Foundation
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
